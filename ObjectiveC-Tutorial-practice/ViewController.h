d85eb51d1d2903fda43ab012df08c3f11d5c6ef3
urmil <ucodes28@gmail.com>
2020-09-25 16:09:47 -0500

    added more labels for int float and double value types

--- ObjectiveC-Tutorial-practice/ViewController.h
+++ ObjectiveC-Tutorial-practice/ViewController.h
@@ -11,12 +11,30 @@
 @interface ViewController : UIViewController {
     
     NSString *name;
+    
+    int finalScore;
+    
+    
+    //declaring the variables
+    
+    float float1;
+    float float2;
+    
+    float myWeight;
+    float myIdealWeight;
 }
     
 
+//Dragging the Outlets
+
+
 @property (weak, nonatomic) IBOutlet UILabel *label;
 
+@property (weak, nonatomic) IBOutlet UILabel *intLabel;
 
+@property (weak, nonatomic) IBOutlet UILabel *label3;
+
+@property (weak, nonatomic) IBOutlet UILabel *label4;
 
 
 @end
--- ObjectiveC-Tutorial-practice/ViewController.m
+++ ObjectiveC-Tutorial-practice/ViewController.m
@@ -22,9 +22,27 @@
     name = @"Urmil";
     
     NSString *word = @"Hello";
+    
+    finalScore = 4;
+    
+    float1 = 100.2222;
+    float2 = 789.3333;
+    
+    myWeight = 84.5;
+    myIdealWeight = 71.00;
+    
+    float calc1 = float1 + float2;
+    float totalWeight = myWeight + myIdealWeight;
+    
 
     self.label.text = [NSString stringWithFormat:@"%@ %@", word, name];
     
+    self.intLabel.text = [NSString stringWithFormat:@"%d", finalScore];
+ 
+    self.label3.text = [NSString stringWithFormat:@"%.4f", calc1];
+    
+    self.label4.text = [NSString stringWithFormat:@"%.1f", totalWeight];
+
 }
 
 
